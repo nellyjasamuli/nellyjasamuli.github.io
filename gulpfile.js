7a039c87e42e011fd2ce48e40c4c4c72b20f208c
Samuli Tamminen <samuli.tamminen@gmail.com>
2016-03-17 22:47:16 +0200

    setup gulp

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp'),
+  sass = require('gulp-ruby-sass'),
+  cssnano = require('gulp-cssnano'),
+  uglify = require('gulp-uglify'),
+  imagemin = require('gulp-imagemin'),
+  rename = require('gulp-rename'),
+  concat = require('gulp-concat'),
+  cache = require('gulp-cache'),
+  del = require('del');
+
+
+var sassOptions = { style: 'expanded' };
+
+// Styles
+gulp.task('styles', function() {
+  return sass('assets/sass/*.scss', sassOptions)
+    .pipe(gulp.dest('assets/css'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(cssnano())
+    .pipe(gulp.dest('assets/css'));
+});
+
+
+// Scripts
+gulp.task('scripts', function() {
+  return gulp.src('assets/js/*.js')
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('assets/js'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/js'));
+});
+
+
+// Clean up
+gulp.task('clean', function() {
+    return del(['assets/css/ie8.css', 'assets/css/main.css', 'assets/js/bundle.js']);
+});
+
+
+// Default task
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts');
+});
+
+
+// Watch files
+gulp.task('watch', function() {
+
+  // Watch .scss files
+  gulp.watch('assets/sass/*.scss', ['styles']);
+
+  // Watch .js files
+  gulp.watch('assets/js/*.js', ['scripts']);
+
+});
